eea235cdbc28f3cf84168d32b641aee46d03bf33
python <rabindrachapai14@gmail.com>
2025-10-20 10:33:56 +0545

    setting

--- rujumee/settings.py
+++ rujumee/settings.py
@@ -123,4 +123,4 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static/')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CSRF_TRUSTED_ORIGINS = ['https://yourdomain.com']
+CSRF_TRUSTED_ORIGINS = ['https://rabindrachapain.com.np']
